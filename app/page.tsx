99a761f0d6358d240d99ef7cad06bc46bf9e180b
*PrateekBanwari* <*prateekbanwari0@gmail.com*>
2026-09-15 23:39:07 +0530

    implemented login ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="bg-ring"> hello
-   </div>
+   <div className="fixed inset-0 pointer-events-none overflow-hidden z-0">
+        <div className="absolute -top-40 -left-40 w-96 h-96 bg-muted rounded-full blur-3xl" />
+        <div className="absolute top-1/2 -right-40 w-120 h-120 bg-cyan-500/10 rounded-full blur-3xl" />
+        <div className="absolute bottom-0 left-1/3 w-80 h-80 bg-blue-600/10 rounded-full blur-3xl" />
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#1f293d15_1px,transparent_1px),linear-gradient(to_bottom,#1f293d15_1px,transparent_1px)] bg-size-[32px_32px]" />
+      </div>
+   
   );
 }
