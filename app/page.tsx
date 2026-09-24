c2f3b49bb803ea56b49f629d16f3744e01ffd51f
*PrateekBanwari* <*prateekbanwari0@gmail.com*>
2026-09-24 22:35:06 +0530

    implemented protected routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { requireAuth } from "@/module/auth/utils/auth_utils";
 import { redirect } from "next/navigation";
 
 export const instant = false;
 
 export default function Home() {
+   requireAuth();
    return redirect("/dashboard");
 
 }
